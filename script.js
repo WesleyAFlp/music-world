cd88ab19355d6c0614ab151dcf32cbd08cec7595
Wesley Amaral de Carvalho <0001155125@senaimgaluno.com.br>
2026-03-12 08:40:34 -0300

    Início do script

---
+++ script.js
@@ -0,0 +1,16 @@
+/* ======================================================================================
+                            SCRIPT DO SISTEMA
+   ======================================================================================*/
+
+// Display a welcome message with an alert via a button
+document.getElementById("bMessage").addEventListener("click", function(){
+    alert("Bem-vindo(a) ao nosso site!");
+})
+
+// Changing one message with button
+function changingMessage(){
+    let newMessage = "Esse site possui um script!"
+    document.getElementById("newMessage").addEventListener("click", function(){
+
+    })
+}
